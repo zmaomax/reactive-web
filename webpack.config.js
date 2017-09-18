6fbaa14b742a0cdacc9cb714613f96c53ded0976
Mauricio Florez <mauricioflorez@ZCOBTMH100242Max.local>
2017-09-18 10:37:58 -0500

    Added react app structure

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './js/reactiveWebApp.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer:{
+    publicPath : '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
